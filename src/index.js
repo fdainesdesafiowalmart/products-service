519d5d29af296e85d0670406cdbd791e6e1add98
Francisco Daines <francisco.daines@2brains.cl>
2020-08-18 21:39:16 -0400

    Adds express middlewares and exposes healthcheck endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const port = process.env.PORT | 3000
 
 const app = express()
+app.use(cors())
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/health', (req, res) => {
+  res.status(200).send()
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
