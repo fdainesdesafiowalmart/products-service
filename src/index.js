a8335e7e6c6abc2b7523b71bbdd45d09840b75a4
Francisco Daines <francisco.daines@2brains.cl>
2020-08-18 21:27:09 -0400

    Adds a very basic endpoint to return a Hello World message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hola Mundo')
+const express = require('express')
+const port = process.env.PORT | 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
